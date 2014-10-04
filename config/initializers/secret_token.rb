5197395f8afdc2b4dc2fde378223e8b3cb9158b8
kentana20 <civic.b16b@gmail.com>
2014-10-04 17:08:33 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PremierLeagueInfo::Application.config.secret_key_base = 'cb06ff17cc7cdd7160edc4cd845c4c9a67e91ca0c5e43287f41c881685739fce9bb0a7a12ea2fec309821a70e92de51823a1302d43983f272c681e3d29441d56'
